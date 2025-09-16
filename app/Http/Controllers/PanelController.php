1c2546a1b60ff9fb9e40fa12f921e1b110cb705c
mrwhy224 <mail@mroohi.ir>
2025-09-16 19:59:00 +0330

    add blank controller

---
+++ app/Http/Controllers/PanelController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PanelController
+{
+    public function index()
+    {
+        return view('panel.home');
+    }
+}
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PostController
+{
+    public function list()
+    {
+        return view('panel.posts');
+    }
+    public function add()
+    {
+        return view('panel.posts');
+    }
+    public function category()
+    {
+        return view('panel.posts');
+    }
+    public function comment()
+    {
+        return view('panel.posts');
+    }
+}
