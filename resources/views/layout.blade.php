8fa0adecd7bf185265aaf839571180ac77269791
mrwhy224 <mail@mroohi.ir>
2025-10-13 21:30:39 +0330

    add favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" type="image/x-icon" href="/favicon.png">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'وب‌سایت آموزشگاه')</title>
     <!-- Tailwind CSS -->
