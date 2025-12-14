c5e59387222b4989f64063d3c1d343ef793fe24a
mrwhy224 <mail@mroohi.ir>
2025-12-14 13:37:13 +0330

    Update UserQuizController.php

--- app/Http/Controllers/UserQuizController.php
+++ app/Http/Controllers/UserQuizController.php
@@ -12,11 +12,7 @@ class UserQuizController
         if (!Auth::check())
             return redirect()->route('register', ['redirect_to' => url()->current()]);
         $quiz->load(['questions.options']);
-        dd([
-            'Quiz Attributes' => $quiz->getAttributes(), // نام ستون‌های کوییز را نشان می‌دهد
-            'First Question' => $quiz->questions->first(), // اولین سوال را (اگر باشد) نشان می‌دهد
-            'Raw Relations' => $quiz->relations,
-        ]);
+        
 
         $quizData = [
             'title' => $quiz->title,
